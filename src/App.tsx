22317eb2396d6988632b5c155fa78b14f9167ebf
Matt Newbigging <matt.newbigging@aize.io>
2023-10-13 14:00:28 +0100

    removed mobx code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./app.scss";
 
 import React from "react";
-import { observer } from "mobx-react-lite";
 
 import { AppState } from "./app-state";
 
@@ -9,10 +8,10 @@ interface AppProps {
   appState: AppState;
 }
 
-export const App: React.FC<AppProps> = observer(({ appState }) => {
+export const App: React.FC<AppProps> = ({ appState }) => {
   return (
     <div className="app">
       <canvas id="canvas"></canvas>
     </div>
   );
-});
+};
